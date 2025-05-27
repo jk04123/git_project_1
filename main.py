60440bf839fd4b5753efb004f0729eb2c2f72511
Yonggurl <jk04123@naver.com>
2025-05-27 14:17:32 +0900

    계산기 메인 함수 수정

--- main.py
+++ main.py
@@ -3,22 +3,19 @@ from subtract import subtract
 from multiply import multiply
 from divide import divide
 def calculator():
- a = float(input("첫 번째 숫자: "))
- b = float(input("두 번째 숫자: "))
- op = input("연산자 (+, -, *, /): ")
- operations = {
- '+': add,
- '-': subtract,
- '*': multiply,
- '/': divide
- }
-if op in operations:
-try:
-result = operations[op](a, b)
-print("결과:", result)
-except Exception as e:
-print("오류:", e)
-else:
-print("올바르지 않은 연산자입니다.")
+    a = float(input("1st number: "))
+    b = float(input("2nd number: "))
+    op = input("연산자 (+, -, *, /): ")
+    operations = {'+': add,
+                  '-': subtract,
+                  '*': multiply,
+                  '/': divide}
+    if op in operations:
+        try:
+            result = operations[op](a, b)
+            print("result:", result)
+        except Exception as e: print("오류:", e)
+    else: print("Invalid operator.")
+
 if __name__ == "__main__":
-calculator()
+    calculator()
